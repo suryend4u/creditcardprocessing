3f9f09d1c3d333ca762da3c0480d814d65ef42bd
Das <suryendu.das@accenture.com>
2020-03-01 02:28:55 +0000

    CARD-3: SSL Configuration

---
+++ src/main/java/com/surya/cards/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.surya.cards.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+
+				.cors()
+				.and()
+				.csrf().disable()
+				.requiresChannel()
+				.anyRequest()
+				.requiresSecure()
+				.and()
+				.authorizeRequests()
+				.antMatchers("/**")
+				.permitAll();
+	}
+}
